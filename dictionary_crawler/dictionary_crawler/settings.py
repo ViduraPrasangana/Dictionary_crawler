adc6d116f57ebb215486416cd20e8afd38c107e6
Vidura Prasangana <vidura.prasangana16@gmail.com>
2024-08-25 14:24:28 +0530

    fix user agent issue

--- dictionary_crawler/dictionary_crawler/settings.py
+++ dictionary_crawler/dictionary_crawler/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'dictionary_crawler.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'dictionary_crawler (+http://www.yourdomain.com)'
-
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 FEED_EXPORT_ENCODING = "utf-8"
